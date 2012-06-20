b8177b44cabd02b7cbe8c63484dbd081b189e7f6
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-06-20 17:29:32 +0200

    Initial version.

---
+++ lib/nanoc/data_sources/bibtex_data_source.rb
@@ -0,0 +1,41 @@
+require 'bibtex'
+
+class BibtexDataSource < Nanoc3::DataSource
+  identifier :bibtex
+  
+  def up
+    @files = Dir[@config[:path].sub(/\/?$/, '/*.bib')];
+    @exclude = {}
+    (@config[:exclude] || []).each { |value| @exclude[value] = true }
+  end
+  
+  def items
+    @files.map do |file|
+      entries = BibTeX.open(file)
+      entries.map do |entry|
+        Nanoc3::Item.new to_bibtex(entry), extract_attributes(entry), entry.key
+      end
+    end.flatten!
+  end
+
+  def to_bibtex entry
+    contents = [ "@#{entry.type}{#{entry.key}," ]
+    entry.each do |key, value|
+      unless @exclude[key.to_s]
+        unless value =~ /\s|\{|\}|"|,|:/
+          contents.push "  #{key} = #{value},"
+        else
+          contents.push "  #{key} = {#{value}},"
+        end
+      end
+    end
+    contents.push "}"
+    contents.join "\n"
+  end
+  
+  def extract_attributes entry
+    attributes = { :key => entry.key }
+    entry.each { |key, value| attributes[key] = value.convert(:latex) }
+    attributes
+  end
+end
