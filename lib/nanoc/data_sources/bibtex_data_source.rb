4ffe12fea88a661d8bece82a022e1ba8b646f933
Ruben Verborgh <ruben@verborgh.org>
2015-12-29 16:34:05 +0100

    Upgrade to Nanoc 4.

--- lib/nanoc/data_sources/bibtex_data_source.rb
+++ lib/nanoc/data_sources/bibtex_data_source.rb
@@ -1,25 +1,25 @@
 require 'bibtex'
 
-class BibtexDataSource < Nanoc3::DataSource
+class BibtexDataSource < Nanoc::DataSource
   identifier :bibtex
-  
+
   def up
     @files = Dir[@config[:path].sub(/\/?$/, '/*.bib')];
     @exclude = {}
     (@config[:exclude] || []).each { |value| @exclude[value] = true }
   end
-  
+
   def items
     @files.map do |file|
       entries = BibTeX.open(file)
       entries.map do |entry|
-        Nanoc3::Item.new to_bibtex(entry), extract_attributes(entry), entry.key
+        new_item to_bibtex(entry), extract_attributes(entry), "/#{entry.key}"
       end
     end.flatten!
   end
 
   def to_bibtex entry
-    contents = [ "@#{entry.type}{#{entry.key}," ]
+    contents = ["@#{entry.type}{#{entry.key},"]
     entry.each do |key, value|
       unless @exclude[key.to_s]
         if value =~ /^\d+$/ or (key == :month and value =~ /^[a-z]{1,3}$/)
@@ -32,7 +32,7 @@ class BibtexDataSource < Nanoc3::DataSource
     contents.push "}"
     contents.join "\n"
   end
-  
+
   def extract_attributes entry
     attributes = { :key => entry.key }
     entry.each { |key, value| attributes[key] = value.convert(:latex) }
